23cd39412db3bc1869962d3e93c595434e12e1c5
shivanand-k <shivanandcrew034@gmail.com>
2025-05-03 17:47:33 +0530

    config is fixed

--- client/src/pages/TaskDetail.jsx
+++ client/src/pages/TaskDetail.jsx
@@ -31,6 +31,22 @@ export default function TaskDetail(){
         getTask();
     } , [taskId]);
 
+    const handleDelete = async(e , taskId)=>{
+        e.preventDefault();
+        setIsLoading(true);
+        try {
+            const response = await getAndDeleteReq(`${baseUrl}/task/${taskId}` , "delete");
+            if(response.status === "success"){
+                toast.success("tasks is deleted! ");
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || "server Error! ";
+            toast.error(errorMessage)
+        }finally{
+            setIsLoading(false);
+        }
+    }
+
     return(
         <div className="min-h-screen flex justify-center items-center flex-col gap-2.5">
             <h1 className="text-lg font-bold">Task-Detail</h1>
@@ -56,7 +72,7 @@ export default function TaskDetail(){
                             <div className="flex flex-wrap justify-around items-center gap-1.5">
                                 <button className="btn btn-neutral shadow-lg">Assign</button>
                                 <Link to={`/edittask/${task._id}`}><button className="btn btn-neutral shadow-lg">Edit</button></Link>
-                                <button className="btn btn-neutral shadow-lg">Delete</button>
+                                <button className="btn btn-neutral shadow-lg" onClick={handleDelete}>Delete</button>
                             </div>
                         </>
                     ) : (<p>Task Not Found!</p>)
